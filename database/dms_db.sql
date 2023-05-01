f0527ff046503d3e86ae853447f68dfefff6c575
nty2001 <thuyen01102001@gmail.com>
2023-05-01 10:36:26 +0700

    fix

--- database/dms_db.sql
+++ database/dms_db.sql
@@ -1,24 +1,30 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 07, 2022 at 09:32 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.5
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th5 01, 2023 lúc 05:36 AM
+-- Phiên bản máy phục vụ: 10.4.28-MariaDB
+-- Phiên bản PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Database: `dms_db`
+-- Cơ sở dữ liệu: `dms_db`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `account_list`
+-- Cấu trúc bảng cho bảng `account_list`
 --
 
 CREATE TABLE `account_list` (
@@ -31,10 +37,10 @@ CREATE TABLE `account_list` (
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `account_list`
+-- Đang đổ dữ liệu cho bảng `account_list`
 --
 
 INSERT INTO `account_list` (`id`, `code`, `student_id`, `room_id`, `rate`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
@@ -44,7 +50,7 @@ INSERT INTO `account_list` (`id`, `code`, `student_id`, `room_id`, `rate`, `stat
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dorm_list`
+-- Cấu trúc bảng cho bảng `dorm_list`
 --
 
 CREATE TABLE `dorm_list` (
@@ -54,23 +60,23 @@ CREATE TABLE `dorm_list` (
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `dorm_list`
+-- Đang đổ dữ liệu cho bảng `dorm_list`
 --
 
 INSERT INTO `dorm_list` (`id`, `name`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
-(1, 'Male Dorm 1', 1, 0, '2022-05-07 10:07:55', '2022-05-07 10:08:04'),
-(2, 'Female Dorm 1', 1, 0, '2022-05-07 10:08:15', '2022-05-07 10:08:15'),
-(3, 'Male Dorm 2', 1, 0, '2022-05-07 10:08:32', '2022-05-07 10:08:32'),
-(4, 'Female Dorm 2', 1, 0, '2022-05-07 10:08:41', '2022-05-07 10:08:41'),
-(5, 'Male Dorm 101', 0, 0, '2022-05-07 10:09:45', '2022-05-07 10:09:45');
+(1, 'Khu A', 1, 0, '2022-05-07 10:07:55', '2023-04-30 21:17:10'),
+(2, 'Khu B', 1, 0, '2022-05-07 10:08:15', '2023-04-30 21:17:32'),
+(3, 'Khu A', 1, 0, '2022-05-07 10:08:32', '2023-04-30 21:17:56'),
+(4, ' túc', 1, 0, '2022-05-07 10:08:41', '2023-05-01 10:17:09'),
+(5, 'Male Dorm 101', 0, 1, '2022-05-07 10:09:45', '2023-05-01 10:17:16');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `payment_list`
+-- Cấu trúc bảng cho bảng `payment_list`
 --
 
 CREATE TABLE `payment_list` (
@@ -80,10 +86,10 @@ CREATE TABLE `payment_list` (
   `amount` float(12,2) NOT NULL DEFAULT 0.00,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `payment_list`
+-- Đang đổ dữ liệu cho bảng `payment_list`
 --
 
 INSERT INTO `payment_list` (`id`, `account_id`, `month_of`, `amount`, `date_created`, `date_updated`) VALUES
@@ -94,7 +100,7 @@ INSERT INTO `payment_list` (`id`, `account_id`, `month_of`, `amount`, `date_crea
 -- --------------------------------------------------------
 
 --
--- Table structure for table `room_list`
+-- Cấu trúc bảng cho bảng `room_list`
 --
 
 CREATE TABLE `room_list` (
@@ -107,25 +113,25 @@ CREATE TABLE `room_list` (
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `room_list`
+-- Đang đổ dữ liệu cho bảng `room_list`
 --
 
 INSERT INTO `room_list` (`id`, `dorm_id`, `name`, `slots`, `price`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
-(1, 1, 'Room 101', 4, 3500.00, 1, 0, '2022-05-07 10:35:39', '2022-05-07 10:35:39'),
-(2, 1, 'Room 102', 4, 3500.00, 1, 0, '2022-05-07 10:35:53', '2022-05-07 10:35:53'),
-(3, 2, 'Room 101', 4, 3500.00, 1, 0, '2022-05-07 10:36:08', '2022-05-07 10:36:08'),
-(4, 2, 'Room 102', 4, 3500.00, 1, 0, '2022-05-07 10:36:19', '2022-05-07 10:36:19'),
-(5, 3, 'Room 101', 2, 5000.00, 1, 0, '2022-05-07 10:36:34', '2022-05-07 10:36:34'),
-(6, 4, 'Room 101', 2, 5000.00, 1, 0, '2022-05-07 10:36:43', '2022-05-07 10:36:43'),
-(7, 2, 'Room 103', 6, 1000.00, 0, 0, '2022-05-07 10:37:20', '2022-05-07 10:37:20');
+(1, 1, 'A101', 4, 3500.00, 1, 0, '2022-05-07 10:35:39', '2023-04-30 21:19:03'),
+(2, 1, 'A102', 4, 3500.00, 1, 0, '2022-05-07 10:35:53', '2023-04-30 21:20:48'),
+(3, 2, 'B102', 4, 3500.00, 1, 0, '2022-05-07 10:36:08', '2023-04-30 21:21:00'),
+(4, 2, 'A103', 4, 3500.00, 1, 0, '2022-05-07 10:36:19', '2023-04-30 21:21:15'),
+(5, 3, 'B101', 2, 5000.00, 1, 0, '2022-05-07 10:36:34', '2023-04-30 21:21:19'),
+(6, 4, 'A104', 2, 5000.00, 1, 0, '2022-05-07 10:36:43', '2023-04-30 21:21:28'),
+(7, 2, 'B103', 6, 1000.00, 0, 0, '2022-05-07 10:37:20', '2023-04-30 21:21:43');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `student_list`
+-- Cấu trúc bảng cho bảng `student_list`
 --
 
 CREATE TABLE `student_list` (
@@ -148,35 +154,35 @@ CREATE TABLE `student_list` (
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `student_list`
+-- Đang đổ dữ liệu cho bảng `student_list`
 --
 
 INSERT INTO `student_list` (`id`, `code`, `firstname`, `middlename`, `lastname`, `department`, `course`, `gender`, `contact`, `email`, `address`, `emergency_name`, `emergency_contact`, `emergency_address`, `emergency_relation`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
-(1, '6231415', 'Mark', 'D', 'Cooper', 'College of Engineering', 'Bachelor of Science in Computer  Science', 'Male', '09123456789', 'mcooper@gmail.com', '23 St, Here City, Over There Province,  2306', 'Kevin Cooper', '09654789123', '23 St, Here City, Over There Province,  2306', 'Father', 1, 0, '2022-05-07 11:12:38', '2022-05-07 11:12:38'),
-(2, '101497', 'Claire', 'C', 'Blake', 'College of Social Science', 'Bachelor of Science in Psychology', 'Female', '09654789123', 'cblake@gmail.com', '15 St., There City, Anywhere Province, 1014', 'Cynthia Blake', '09123456987', '15 St., There City, Anywhere Province, 1014', 'Mother', 1, 0, '2022-05-07 11:17:22', '2022-05-07 11:17:22');
+(1, '1921500673', 'Ngô Thị ', 'Thị', 'Ánh Tuyết', 'Trường đại học Mỏ-Địa chất', 'Công nghệ thông tin', 'Nữ', '09123456789', 'mcooper@gmail.com', '23 St, Here City, Over There Province,  2306', 'Kevin Cooper', '09654789123', '23 St, Here City, Over There Province,  2306', 'Father', 1, 0, '2022-05-07 11:12:38', '2023-04-30 21:09:51'),
+(2, '1921050803', 'Dương', 'Như', 'Quỳnh', 'Trường đại học Mỏ-Địa chất', 'Cơ điện', 'Male', '09654789123', 'cblake@gmail.com', 'Bắc giang', '..', '09123456987', 'Bắc giang', 'Mẹ', 1, 0, '2022-05-07 11:17:22', '2023-04-30 22:21:27');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `system_info`
+-- Cấu trúc bảng cho bảng `system_info`
 --
 
 CREATE TABLE `system_info` (
   `id` int(30) NOT NULL,
   `meta_field` text NOT NULL,
   `meta_value` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `system_info`
+-- Đang đổ dữ liệu cho bảng `system_info`
 --
 
 INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
-(1, 'name', 'School Dormitory Management System'),
-(6, 'short_name', 'DMS - PHP'),
+(1, 'name', 'Quản lý kí túc xá trường đại học Mỏ-Địa chất'),
+(6, 'short_name', 'QLKTX-HUMG'),
 (11, 'logo', 'uploads/logo.png?v=1651888584'),
 (13, 'user_avatar', 'uploads/user_avatar.jpg'),
 (14, 'cover', 'uploads/cover.png?v=1651888585');
@@ -184,7 +190,7 @@ INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Cấu trúc bảng cho bảng `users`
 --
 
 CREATE TABLE `users` (
@@ -199,22 +205,22 @@ CREATE TABLE `users` (
   `type` tinyint(1) NOT NULL DEFAULT 0,
   `date_added` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Đang đổ dữ liệu cho bảng `users`
 --
 
 INSERT INTO `users` (`id`, `firstname`, `middlename`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `date_added`, `date_updated`) VALUES
-(1, 'Adminstrator', NULL, 'Admin', 'admin', '0192023a7bbd73250516f069df18b500', 'uploads/avatars/1.png?v=1649834664', NULL, 1, '2021-01-20 14:02:37', '2022-04-13 15:24:24'),
-(5, 'Mike', 'C', 'Williamns', 'mwilliams', 'a88df23ac492e6e2782df6586a0c645f', 'uploads/avatars/5.png?v=1651908504', NULL, 2, '2022-05-07 15:28:24', '2022-05-07 15:28:24');
+(1, 'Quản trị viên', '', 'Yến Na', 'yenna0110', '74228bb3c6baecea461825d938e6141a', 'uploads/avatars/1.png?v=1649834664', NULL, 1, '2021-01-20 14:02:37', '2023-04-30 21:41:33'),
+(5, 'Nhân viên', 'C', 'Quynh', 'quynh2108', 'c34717bc9b4d638619fd88cc718dd442', 'uploads/avatars/5.png?v=1651908504', NULL, 2, '2022-05-07 15:28:24', '2023-04-30 21:01:48');
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `account_list`
+-- Chỉ mục cho bảng `account_list`
 --
 ALTER TABLE `account_list`
   ADD PRIMARY KEY (`id`),
@@ -222,109 +228,113 @@ ALTER TABLE `account_list`
   ADD KEY `room_id` (`room_id`);
 
 --
--- Indexes for table `dorm_list`
+-- Chỉ mục cho bảng `dorm_list`
 --
 ALTER TABLE `dorm_list`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `payment_list`
+-- Chỉ mục cho bảng `payment_list`
 --
 ALTER TABLE `payment_list`
   ADD PRIMARY KEY (`id`),
   ADD KEY `account_id` (`account_id`);
 
 --
--- Indexes for table `room_list`
+-- Chỉ mục cho bảng `room_list`
 --
 ALTER TABLE `room_list`
   ADD PRIMARY KEY (`id`),
   ADD KEY `dorm_id` (`dorm_id`);
 
 --
--- Indexes for table `student_list`
+-- Chỉ mục cho bảng `student_list`
 --
 ALTER TABLE `student_list`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `system_info`
+-- Chỉ mục cho bảng `system_info`
 --
 ALTER TABLE `system_info`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Chỉ mục cho bảng `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `account_list`
+-- AUTO_INCREMENT cho bảng `account_list`
 --
 ALTER TABLE `account_list`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `dorm_list`
+-- AUTO_INCREMENT cho bảng `dorm_list`
 --
 ALTER TABLE `dorm_list`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `payment_list`
+-- AUTO_INCREMENT cho bảng `payment_list`
 --
 ALTER TABLE `payment_list`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `room_list`
+-- AUTO_INCREMENT cho bảng `room_list`
 --
 ALTER TABLE `room_list`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `student_list`
+-- AUTO_INCREMENT cho bảng `student_list`
 --
 ALTER TABLE `student_list`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `system_info`
+-- AUTO_INCREMENT cho bảng `system_info`
 --
 ALTER TABLE `system_info`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- Constraints for dumped tables
+-- Các ràng buộc cho các bảng đã đổ
 --
 
 --
--- Constraints for table `account_list`
+-- Các ràng buộc cho bảng `account_list`
 --
 ALTER TABLE `account_list`
   ADD CONSTRAINT `room_id_fk_al` FOREIGN KEY (`room_id`) REFERENCES `room_list` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   ADD CONSTRAINT `student_id_fk_al` FOREIGN KEY (`student_id`) REFERENCES `student_list` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
--- Constraints for table `payment_list`
+-- Các ràng buộc cho bảng `payment_list`
 --
 ALTER TABLE `payment_list`
   ADD CONSTRAINT `account_id_fk_pl` FOREIGN KEY (`account_id`) REFERENCES `account_list` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
--- Constraints for table `room_list`
+-- Các ràng buộc cho bảng `room_list`
 --
 ALTER TABLE `room_list`
   ADD CONSTRAINT `drom_id_fk_rl` FOREIGN KEY (`dorm_id`) REFERENCES `dorm_list` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
